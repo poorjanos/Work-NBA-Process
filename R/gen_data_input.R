4b2f62cd27e32c899619488dec85bc657b9dba74
Janos Poor <poorjanos@gmail.com>
2018-06-04 15:09:24 +0200

    Extend ui for app

---
+++ R/gen_data_input.R
@@ -0,0 +1,90 @@
+# Data generation pipline to feed app
+# Load required libs
+library(config)
+library(here)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(lubridate)
+library(bupaR)
+library(processmapR)
+library(processmonitR)
+
+#########################################################################################
+# Data Extraction #######################################################################
+#########################################################################################
+
+# Set JAVA_HOME, set max. memory, and load rJava library
+Sys.setenv(JAVA_HOME = "C:\\Program Files\\Java\\jre1.8.0_171")
+options(java.parameters = "-Xmx2g")
+library(rJava)
+
+# Output Java version
+.jinit()
+print(.jcall("java/lang/System", "S", "getProperty", "java.version"))
+
+# Load RJDBC library
+library(RJDBC)
+
+# Get credentials
+kontakt <-
+  config::get("kontakt", file = "C:\\Users\\PoorJ\\Projects\\config.yml")
+
+# Create connection driver
+jdbcDriver <-
+  JDBC(driverClass = "oracle.jdbc.OracleDriver", classPath = "C:\\Users\\PoorJ\\Desktop\\ojdbc7.jar")
+
+# Open connection: kontakt---------------------------------------------------------------
+jdbcConnection <-
+  dbConnect(
+    jdbcDriver,
+    url = kontakt$server,
+    user = kontakt$uid,
+    password = kontakt$pwd
+  )
+
+# Fetch data
+query <- "select * from t_newbusiness_event_log"
+t_newbusiness_event_log <- dbGetQuery(jdbcConnection, query)
+
+# Close db connection: kontakt
+dbDisconnect(jdbcConnection)
+
+
+#########################################################################################
+# Data Transformation ###################################################################
+#########################################################################################
+
+t_event_log <- t_newbusiness_event_log %>%
+  mutate(ACTIVITY_INST_ID = as.numeric(row.names(.)),
+         CONTRACT_PERIOD = paste0(substr(CONTRACT_PERIOD, 1, 4), "/", substr(CONTRACT_PERIOD, 6, 7))) %>%
+  gather(
+    LIFECYCLE_ID,
+    TIMESTAMP,
+    -CASE_ID,
+    -EVENT_NAME,
+    -EVENT_NAME_HU,
+    -PRODUCT_CODE,
+    -PRODUCT_LINE,
+    -PARTNER_CODE,
+    -PARTNER_NAME,
+    -KTI_CODE,
+    -KTI_NAME,
+    -SALES_CHANNEL_CODE,
+    -SALES_CHANNEL,
+    -MEDIUM_TYPE,
+    -AUTOUW,
+    -CONTRACT_PERIOD,
+    -ACTIVITY_INST_ID
+  ) %>%
+  mutate(TIMESTAMP = ymd_hms(TIMESTAMP)) %>%
+  arrange(CASE_ID, TIMESTAMP) %>%
+  mutate(LIFECYCLE_ID = case_when(
+    .$LIFECYCLE_ID == "EVENT_BEGIN" ~ "START",
+    TRUE ~ "END"
+  ))
+
+# Save to local storage
+write.csv(t_event_log %>% filter(CONTRACT_PERIOD == '2018/01'),
+          here::here("Data", "t_event_log_201801.csv"),
+          row.names = FALSE)
--- app.R
+++ app.R
@@ -7,44 +7,72 @@ library(DiagrammeR)
 
 
 t_event_log_app <- read.csv(here::here("Data", "t_event_log_201801.csv"), stringsAsFactors = FALSE) %>%
-  mutate(TIMESTAMP = ymd_hms(TIMESTAMP))
+  mutate(
+    TIMESTAMP = ymd_hms(TIMESTAMP),
+    PRODUCT_LINE = as.factor(PRODUCT_LINE),
+    SALES_CHANNEL_CODE = as.factor(SALES_CHANNEL_CODE),
+    MEDIUM_TYPE = as.factor(MEDIUM_TYPE),
+    AUTOUW = as.factor(case_when(
+      .$AUTOUW == "I" ~ "Automatikus",
+      TRUE ~ "Manuális"
+    ))
+  )
 
 ui <- fluidPage(
-  titlePanel("New Business Acquisition Cycle"),
+  titlePanel("Kötvényesítés: aláírástól jutlékfizetésig"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("traceFreqInput", "Trace Freq", min = 0, max = 1, value = 0.5),
-      selectInput("prodLineInput", "Product Line", choices = c("Home", "TPML", "Life", "Casco")),
-      selectInput("autoUwInput", "Automated Process", choices = c("I", "N"))
+      sliderInput("traceFreqInput", "Szálgyakoriság (trace frequency)", min = 0, max = 1, value = 0.25),
+      checkboxGroupInput("prodLineInput", "Termékkategóriák:",
+        choices = levels(t_event_log_app$PRODUCT_LINE),
+        selected = "Home"
+      ),
+      checkboxGroupInput("autoUwInput", "Automatikus/manuális folyamatág:",
+        choices = levels(t_event_log_app$AUTOUW),
+        selected = levels(t_event_log_app$AUTOUW)
+      ),
+      checkboxGroupInput("SalesChannelInput", "Értékesítési csatorna:",
+        choices = levels(t_event_log_app$SALES_CHANNEL_CODE),
+        selected = levels(t_event_log_app$SALES_CHANNEL_CODE),
+        inline = TRUE
+      ),
+      checkboxGroupInput("mediumInput", "Kötési mód:",
+        choices = levels(t_event_log_app$MEDIUM_TYPE),
+        selected = levels(t_event_log_app$MEDIUM_TYPE),
+        inline = TRUE
+      ),
+      actionButton("runFilter", "Kérem a folyamarajzot!")
     ),
     mainPanel(grVizOutput("processMap", width = "100%", height = "800px"))
   )
 )
 
 server <- function(input, output) {
-  filtered <- reactive({
-    t_event_log_app %>%
-      filter(
-        PRODUCT_LINE == input$prodLineInput & AUTOUW == input$autoUwInput
-      ) %>%
-      eventlog(
-        case_id = "CASE_ID",
-        activity_id = "EVENT_NAME_HU",
-        activity_instance_id = "ACTIVITY_INST_ID",
-        lifecycle_id = "LIFECYCLE_ID",
-        timestamp = "TIMESTAMP",
-        resource_id = "PARTNER_NAME"
-      ) %>%
-      #filter_endpoints(start_activities = "alairas", end_activities = "jutalek_kifizetes") %>%
-      filter_trace_frequency(percentage = input$traceFreqInput, reverse = F) %>% 
-      filter_activity('enyil_papir_erkezes', reverse = TRUE)
-  })
 
-
+filtered <- eventReactive(input$runFilter, {
+                  t_event_log_app %>%
+                    filter(
+                      PRODUCT_LINE %in% input$prodLineInput & 
+                        AUTOUW %in% input$autoUwInput &
+                        SALES_CHANNEL_CODE %in% input$SalesChannelInput &
+                        MEDIUM_TYPE %in% input$mediumInput
+                    ) %>%
+                    eventlog(
+                      case_id = "CASE_ID",
+                      activity_id = "EVENT_NAME_HU",
+                      activity_instance_id = "ACTIVITY_INST_ID",
+                      lifecycle_id = "LIFECYCLE_ID",
+                      timestamp = "TIMESTAMP",
+                      resource_id = "PARTNER_NAME"
+                    ) %>%
+                    filter_trace_frequency(percentage = input$traceFreqInput, reverse = F) %>%
+                    filter_endpoints(start_activities = "alairas", end_activities = "jutalek_kifizetes") %>% 
+                    filter_activity("enyil_papir_erkezes", reverse = TRUE)
+                })  
+               
   output$processMap <- renderGrViz({
     filtered() %>%
-#      process_map(type_nodes = frequency("absolute"), type_edges = performance(median, "days"), rankdir = "TB")
-    process_map(type_nodes = frequency("relative"), type_edges = frequency("relative"), rankdir = "TB")
+      process_map(type_nodes = frequency("absolute"), type_edges = frequency("absolute"), rankdir = "TB")
   })
 }
 
